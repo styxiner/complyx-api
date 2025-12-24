8cb6cd0a6ccbbd41dd578b073975799658a670e6
Jaime Moranchel <160549592+Jaime-Moranchel@users.noreply.github.com>
2025-12-24 17:10:50 +0100

    estructura inicial

---
+++ src/main/java/com/styxiner/complyx_api/ComplyxApiApplication.java
@@ -0,0 +1,13 @@
+package com.styxiner.complyx_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplyxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplyxApiApplication.class, args);
+	}
+
+}
