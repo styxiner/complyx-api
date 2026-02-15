d56e6225226ab89427c4f0854399c4480e871cc3
styxiner <123456789+styxiner@users.noreply.github.com>
2026-02-15 20:17:48 +0100

    DateUtils created example

---
+++ src/main/java/common/util/DateUtils.java
@@ -0,0 +1,14 @@
+package common.util;
+
+import java.time.Instant;
+
+public class DateUtils {
+	private DateUtils() {
+		
+	}
+	
+	public static Instant nowUtc() {
+		return Instant.now();
+	}
+
+}
