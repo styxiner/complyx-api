b736954b1668c911787f3e81ab7abead7efa7384
styxiner <160549592+styxiner@users.noreply.github.com>
2026-04-24 10:24:05 +0200

    eliminado lombok para usar metodos manuales de clase

--- src/main/java/io/github/styxiner/complyx_api/security/TokenResponseDTO.java
+++ src/main/java/io/github/styxiner/complyx_api/security/TokenResponseDTO.java
@@ -1,15 +1,43 @@
 package io.github.styxiner.complyx_api.security;
 
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class TokenResponseDTO {
 
-    private String accessToken;
+	private String accessToken;
     private String refreshToken;
     private long expiresIn;
+	
+    public TokenResponseDTO(String accessToken, String refreshToken, long expiresIn) {
+		super();
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+		this.expiresIn = expiresIn;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getRefreshToken() {
+		return refreshToken;
+	}
+
+	public void setRefreshToken(String refreshToken) {
+		this.refreshToken = refreshToken;
+	}
+
+	public long getExpiresIn() {
+		return expiresIn;
+	}
+
+	public void setExpiresIn(long expiresIn) {
+		this.expiresIn = expiresIn;
+	}
+    
+    
+    
+    
 }
--- src/main/java/io/github/styxiner/complyx_api/users/UserUpdateDTO.java
+++ src/main/java/io/github/styxiner/complyx_api/users/UserUpdateDTO.java
@@ -3,4 +3,30 @@ package io.github.styxiner.complyx_api.users;
 public class UserUpdateDTO {
 	private String email;
 	private String password;
+	
+	public UserUpdateDTO(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	
+	
 }
