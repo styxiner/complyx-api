e33a8ead7c40f63e345f18a84783c26a49975dfb
styxiner <160549592+styxiner@users.noreply.github.com>
2026-04-24 11:51:48 +0200

    Mejora de filtros de seguridad

--- src/main/java/io/github/styxiner/complyx_api/security/AuthController.java
+++ src/main/java/io/github/styxiner/complyx_api/security/AuthController.java
@@ -1,6 +1,7 @@
 package io.github.styxiner.complyx_api.security;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,26 +23,24 @@ public class AuthController {
         return ResponseEntity.ok(authService.login(dto));
     }
 
-    // Esto no funcionaba 
-/*
-    @PostMapping("/refresh")
-    public ResponseEntity<TokenResponseDTO> refresh(@RequestBody String token) {
-        return ResponseEntity.ok(authService.refreshToken(token));
-    }
-*/    
     @PostMapping("/refresh")
     public ResponseEntity<TokenResponseDTO> refresh(@RequestBody RefreshRequestDTO dto) {
         return ResponseEntity.ok(authService.refreshToken(dto.getRefreshToken()));
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout(@RequestHeader("Authorization") String header) {
+    /*public ResponseEntity<Void> logout(Authentication authentication) {
+    	
+    }*/
 
-    	String token = header.replace("Bearer ", "");
+    // Como los tokens son stateless, no existe de momento un proceso de cierre de sesión. Como mitigación, la duración de los tokens es reducida.
+    public ResponseEntity<Void> logout(@RequestHeader("Authorization") String header) {
     	
     	if (header == null || !header.startsWith("Bearer ")) {
     		return ResponseEntity.badRequest().build();
     	}
+    	
+    	String token = header.replace("Bearer ", "");
         
         authService.logout(token);
 
